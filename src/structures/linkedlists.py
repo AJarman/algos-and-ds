4276555f87d873b967cca9fb8ae5a08647005bda
Andrew Jarman <26150780+AJarman@users.noreply.github.com>
2022-01-08 18:17:41 +0000

    added __len__ method

--- src/structures/linkedlists.py
+++ src/structures/linkedlists.py
@@ -1,5 +1,6 @@
 """Linked List implementations"""
-from typing import Any, Type, Optional
+from typing import Any, Optional, Type
+
 from .nodes import SinglyLinkedNode
 
 
@@ -36,13 +37,15 @@ class LinkedList:
         Raises:
             TypeError: this is raised if new_node is not a SinglyLinkedNode
         """
-        if isinstance(new_node, Type[SinglyLinkedNode]) or new_node is None:
+        if (isinstance(new_node, SinglyLinkedNode)
+            or new_node is None
+                or issubclass(new_node, SinglyLinkedNode)):
             self._head_node = new_node
         else:
             raise TypeError(
                 f"new node: {str(new_node)} is not a compatible Node")
 
-    def insert_beginning(self, new_node_value: Any) -> None:
+    def add_to_head(self, new_node_value: Any) -> None:
         """
         Creates a new head node containing this value,
         updates the new head node to be linked to the old head node.
@@ -55,7 +58,7 @@ class LinkedList:
             value=new_node_value, next_node=self.head_node)
         self.head_node = new_node
 
-    def remove_node(self, value_to_remove:Any)->None:
+    def remove_node(self, value_to_remove: Any) -> None:
         """
         Remove all nodes with this value.
 
@@ -70,15 +73,31 @@ class LinkedList:
         if current_node.value == value_to_remove:
             self.head_node = current_node.next_node
         else:
-            while current_node: # is not None (a)
+            while current_node:  # is not None (a)
                 # look at the next node (b)
-                next_node:Optional[Type[SinglyLinkedNode]] = current_node.next_node
+                next_node: Optional[Type[SinglyLinkedNode]
+                                    ] = current_node.next_node
                 # if it matches the value to remove and is not none
                 if next_node and next_node.value == value_to_remove:
-                # change nodes from a > b > c
-                # to a > c
+                    # change nodes from a > b > c
+                    # to a > c
                     current_node.next_node = next_node.next_node
                     # then start the check from c
                     current_node = next_node.next_node
                 else:
                     current_node = next_node
+
+    def __len__(self)->int:
+        """
+        Calculates number of nodes
+
+        Returns:
+            int: number of nodes
+        """
+        length = 0
+        current_node = self.head_node
+        while current_node:
+            length +=1
+            current_node = current_node.next_node
+
+        return length
